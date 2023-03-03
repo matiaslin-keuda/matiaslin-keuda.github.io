544d99644a3928d20f594e15e9c6373dd2bb5133
matiaslin-keuda <matias.lindela@edu.keuda.fi>
2023-03-03 10:14:55 +0200

    demo6

---
+++ demo6/kirjautuminen.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", onkokirjautunut);
+function onkokirjautunut(){
+    let kirjautunut = 'kylla';
+    if (kijrautunut === 'kylla'){
+       document.getElementById('tervetuloa teksti'). textContent += 'Otto!';
+
+    }
+}
