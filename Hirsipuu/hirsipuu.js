c3fa542721f87ff27de558d77adff7074d5de7e6
matiaslin-keuda <matias.lindela@edu.keuda.fi>
2023-12-13 15:03:39 +0200

    toimiva häviö ja voitto
    2 ääni effectiä
    perus toimminot

--- Hirsipuu/hirsipuu.js
+++ Hirsipuu/hirsipuu.js
@@ -5,24 +5,29 @@ const pilkottu_sana = satunnainen.split('');
 var syotetty_pilkottu = "";
 let vaarin = 0;
 var kuvake = document.getElementById("kuva");
-
+var oikea_aani = new Audio("C:\\Users\\Matias\\OneDrive\\Tiedostot\\matiaslin-keuda.github.io\\Hirsipuu\\kuvat\\ding-sound-effect_1.mp3");
+var vaara_aani =  new Audio("C:\\Users\\Matias\\OneDrive\\Tiedostot\\matiaslin-keuda.github.io\\Hirsipuu\\kuvat\\extremely-loud-incorrect-buzzer_0cDaG20.mp3");
+var voitto_aani = new Audio("C:\\Users\\Matias\\OneDrive\\Tiedostot\\matiaslin-keuda.github.io\\Hirsipuu\\kuvat\\yippee-tbh.mp3")
+var over_aani = new Audio("C:\\Users\\Matias\\OneDrive\\Tiedostot\\matiaslin-keuda.github.io\\Hirsipuu\\kuvat\\super-mario-death-sound-sound-effect.mp3")
 document.addEventListener('keydown', (event) => {
-
+if(vaarin==7){}
+else{
   var name = event.key;
-  var code = event.code;
+
   if (name == "q" || name == "w" || name == "e" || name == "r" || name == "t" || name == "t" || name == "y" || name == "u" || name == "i" || name == "o" || name == "p" || name == "a" || name == "s" || name == "d" || name == "f" || name == "g" || name == "h" || name == "j" || name == "k" || name == "l" ||
     name == "ö" || name == "ä" || name == "z" || name == "x" || name == "c" || name == "v" || name == "b" || name == "n" || name == "m") {
-    // Alert the key name and key code on keydown
-    alert(`nappia painettu ${name} \r\n Key code value: ${code}`);
+
     document.getElementById("confirm").style.color = "black";
     document.getElementById("confirm").innerHTML = "Oletko varma että haluat kokeilla kirjainta " + name.toUpperCase();
-    painet = name
+    painet = name;
+    }
   }
 
 }, false);
 
 function painettu() {
- 
+  if(vaarin==7){}
+  else{
   syotetty_pilkottu = syotetty.split('');
   if (painet != null) {
     if (syotetty_pilkottu.includes(painet)) {
@@ -30,74 +35,112 @@ function painettu() {
       document.getElementById("confirm").innerHTML = "Laitoit kokeiltun kirjaimen"
     }
     else {
-      syotetty += " " + painet
+      syotetty += " " + painet;
+
 
       document.getElementById("kokeiltu").innerHTML = "Testatut Kirjaimet" + syotetty;
 
 
-      document.getElementById("log").innerHTML = pilkottu_sana;
-
-      //jos meni oikein
       if (pilkottu_sana.includes(painet) || syotetty_pilkottu.includes(painet)) {
-      var oikea_aani = new Audio("C:\\Users\\Matias\\OneDrive\\Tiedostot\\matiaslin-keuda.github.io\\Hirsipuu\\kuvat\\ding-sound-effect_1.mp3");
-      oikea_aani.play();
+        syotetty_pilkottu = syotetty.split('');
 
-        alert("oikein");
+        oikea_aani.play();
+       paivitasana();
+        document.getElementById("sana").innerHTML = displayedWord.trim();
+
       }
       else {
-        var vaara_aani = new Audio("C:\\Users\\Matias\\OneDrive\\Tiedostot\\matiaslin-keuda.github.io\\Hirsipuu\\kuvat\\extremely-loud-incorrect-buzzer_0cDaG20.mp3");
-        
-       
         vaarin++
-        switch (vaarin) {
-          case 1:
-            document.getElementById("kuva").style.visibility = "visible";
-            vaara_aani.play();
-            break;
-          case 2:
-            document.getElementById("kuva").src = "kuvat/hirtto3.png";
-            vaara_aani.play();
-            break;
-          case 3:
-            document.getElementById("kuva").src = "kuvat/hirtto5.png";
-            vaara_aani.play();
-            break;
-          case 4:
-            document.getElementById("kuva").src = "kuvat/hirtto7.png";
-            vaara_aani.play();
-            break;
-          case 5:
-            document.getElementById("kuva").src = "kuvat/hirtto9.png";
-            vaara_aani.play();
-            break;
-          case 6:
-            document.getElementById("kuva").src = "kuvat/hirtto11.png";
-            vaara_aani.play();
-            break;
-          case 7:
-            document.getElementById("kuva").src = "kuvat/hirtto12.png";
-            vaara_aani.play();
-            break;
-
-        }
-     //1 3 5 7 9 11 12
-      }
-
-
+        vaara();
+      }}
     }
   }
-}
-function Arpo() {
-  const aluku = Math.floor(Math.random() * 3);
-
-  return aluku
-}
-function Lista() {
-  var lista = new Array("test", "kissa", "joulua")
-  return lista;
-}
-function aloitus() {
-  for (i = 0; i < satunnainen.length; i++) {
-    document.getElementById("sana").innerHTML += " _";
   }
-}
+    function Arpo() {
+      const aluku = Math.floor(Math.random() * 43);
+
+      return aluku
+    }
+    function Lista() {
+      var lista = new Array("kellari", "kissa", "joulu", "tietokone", "kalja", "auto", "kesä", "koulu", "kirja", "aurinko", "metsä", "kukka", "vesi", "musiikki", "kala", "talvi", "lintu", "pöytä", "maailma", "uimaranta", "ilo", "maito", "vuori", "sadetta", "viikko", "runo", "silta", "yö", "viini", "lumi", "ystävä", "kulta", "puu", "hetki", "kukkula", "suukko", "ääni", "ilta", "taivas", "lautta", "ilma", "hevonen", "koti"
+      );
+
+      return lista;
+    }
+    function aloitus() {
+      var initialDisplayedWord = "";
+      for (var i = 0; i < pilkottu_sana.length; i++) {
+        initialDisplayedWord += "_ ";
+      }
+      document.getElementById("sana").innerHTML = initialDisplayedWord.trim();
+    }
+    function paivitasana() {
+      var displayedWord = "";
+      for (var i = 0; i < pilkottu_sana.length; i++) {
+        if (syotetty_pilkottu.includes(pilkottu_sana[i])) {
+          displayedWord += pilkottu_sana[i] + " ";
+        } else {
+          displayedWord += "_ ";
+        }
+      }
+      document.getElementById("sana").innerHTML = displayedWord.trim();
+      if (!displayedWord.includes('_')) {
+        voitto_aani.play();
+        document.getElementById("confirm").innerHTML = "Oikea sana oli: "+satunnainen +" eli arvasit oikein";
+      }
+
+    }
+    aloitus(); syotasana()
+    function syotasana() {
+      var textboxarvo = document.getElementById("textbox").value.toLowerCase();
+      if (textboxarvo === satunnainen.toLowerCase()) {
+        voitto_aani.play();
+        document.getElementById("confirm").innerHTML = "Oikea sana oli: "+satunnainen +" eli arvasit oikein";
+
+      } else {
+        vaarin++;
+        vaara();
+        vaara_aani.play();
+   
+      }
+    }
+    function vaara() {
+      switch (vaarin) {
+        case 1:
+          document.getElementById("kuva").style.visibility = "visible";
+          vaara_aani.play();
+          break;
+        case 2:
+          document.getElementById("kuva").src = "C:\\Users\\Matias\\OneDrive\\Tiedostot\\kuvat\\hirtto3v2.png";
+          vaara_aani.play();
+          break;
+        case 3:
+          document.getElementById("kuva").src = "C:\\Users\\Matias\\OneDrive\\Tiedostot\\kuvat\\hirtto5v2.png";
+          vaara_aani.play();
+          break;
+        case 4:
+          document.getElementById("kuva").src = "C:\\Users\\Matias\\OneDrive\\Tiedostot\\kuvat\\hirtto7v2.png";
+          vaara_aani.play();
+          break;
+        case 5:
+          document.getElementById("kuva").src = "C:\\Users\\Matias\\OneDrive\\Tiedostot\\kuvat\\hirtto9v2.png";
+          vaara_aani.play();
+          break;
+        case 6:
+          document.getElementById("kuva").src = "C:\\Users\\Matias\\OneDrive\\Tiedostot\\kuvat\\hirtto11v2.png";
+          vaara_aani.play();
+          break;
+        case 7:
+          document.getElementById("confirm").innerHTML = "Oikea sana oli: "+satunnainen;
+          document.getElementById("kuva").src = "C:\\Users\\Matias\\OneDrive\\Tiedostot\\kuvat\\hirtto12v2.png";
+          over_aani.play();
+          setTimeout(function() {
+            location.reload();
+        }, 4000);
+          break;
+        
+
+      }
+    }
+      //1 3 5 7 9 11 12
+   
